39caef69061f4300265c4f56bd387719b2f9a542
patrickfrankart <patrick.frankart@gmail.com>
2021-05-12 16:04:09 -0400

    First commit with start of tennis game

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Board extends JFrame {
+
+    public Board() {
+        super("Tennis");
+        this.setSize(300, 300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}
